4d6e9a974799bd602202d89bc15b51d5bf9994cc
xiaoxiong-binggan <2010147613@qq.com>
2026-01-07 23:23:37 +0800

    Delete src directory

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './components/App.vue'
-
-const app = createApp(App)
-const pinia = createPinia()
-
-app.use(pinia)
-app.mount('#app')
